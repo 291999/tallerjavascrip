ef572b2400112e5c8c194e8bb35894b9b324eea9
EQUIPO <EQUIPO@ASUS>
2017-08-06 22:01:32 -0500

    seme dificulto un poco porque no entendia muy vien las etiquetas

---
+++ public_html/P7/P7.js
@@ -0,0 +1,10 @@
+var var1 = parseInt(prompt("enter the value for the var1 "));
+var var2 = parseInt(prompt("enter the value for the var2 "));
+console.log(var1 + "at" + var2 + "is" + var1 ** var2);
+console.log(var2 + "at" + var1 + "is" + var2 ** var1);
+console.log("multiplication between " + var1 + " and " + var2 + " is " + var1 * var2);
+console.log("the division between " + var1 + " and " + var2 + " is " + var1 / var2);
+console.log("the division between " + var2 + "and " + var1 + " is " + var2 / var1);
+console.log("the module of the division between " + var1 + " and " + var2 + " is " + (var1) % (var2));
+console.log("the module of the division between " + var1 + " and " + var2 + " is " + (var1 + var2));
+
