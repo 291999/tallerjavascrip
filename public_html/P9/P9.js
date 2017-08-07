1f0ed214f47f9de1a347fc07f3fea23484a6dd33
EQUIPO <EQUIPO@ASUS>
2017-08-06 22:02:42 -0500

    seme dificulto algo porque la lojica que le aplicava no me dava y porque no conosia algunas etiquetas que podia utilizar

---
+++ public_html/P9/P9.js
@@ -0,0 +1,13 @@
+var num = Number(prompt("ingrese numero"));
+numeroprimo = true;
+for (i = 1; i < num; i++)
+{
+  if (num / i == Math.round(num / i) && i != 1 && i != num) {
+    numeroprimo = false;
+    break;
+  }
+}
+if (numeroprimo)
+  console.log("the number  is cousin");
+else
+  console.log("the number" + num + " " + "he is not cousin");
