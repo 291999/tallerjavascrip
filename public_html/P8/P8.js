f38303167d517d4861a959686b8177bb05b48f2b
EQUIPO <EQUIPO@ASUS>
2017-08-06 22:01:44 -0500

    seme dificulto un poco porque no entendia muy vien las etiquetas

---
+++ public_html/P8/P8.js
@@ -0,0 +1,15 @@
+var number1 = Number(prompt("enter the first number for the operation:"));
+var number2 = Number(prompt("enter the first number for the operation:"));
+
+console.log("whith the number you enter,the following operatons were performed:");
+
+
+
+var resul = (number1 + number2);
+console.log("The sum: " + "IS " + resul);
+var resul = (number1 - number2);
+console.log("subtraction" + " is " + resul);
+var resul = (number1 * number2);
+console.log("multiplication" + " is " + resul);
+var resul = (number1 / number2);
+console.log("divisions" + " is " + resul);
