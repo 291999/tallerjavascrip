832fb91b8080e616278d76477b9112c2470f539f
EQUIPO <EQUIPO@ASUS>
2017-08-06 22:00:38 -0500

    esta finalisado

---
+++ public_html/P4/P4.js
@@ -0,0 +1,14 @@
+var name=(prompt("Ingrese su nombre: "));
+var type=(prompt("Ingrese su tipo de documento: "));
+var number=Number(prompt("Ingrese su numero de identificación: "));
+var gender=(prompt("Ingrese su genero: "));
+var height=Number(prompt("Ingrese su estatura: "));
+var live=(prompt("Es verdad que vive en cartago? "));
+
+console.log("Hola mi nombre es " + name);
+console.log("documento " + type + " numero de documento " + number);
+console.log("Mi genero es " + gender);
+console.log("Mi estatura es " + height + " Mts");
+console.log("es verdad " + live);
+
+
