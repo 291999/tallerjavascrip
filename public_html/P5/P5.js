6bbf1db0ad144bda8d62b1fffd00e3e08a238392
EQUIPO <EQUIPO@ASUS>
2017-08-06 22:00:49 -0500

    esta finalisado

---
+++ public_html/P5/P5.js
@@ -0,0 +1,9 @@
+var name=(prompt("ingrese su nombre: "));
+var type=(prompt("ingrese su tipo de documento: "));
+var number=(prompt("ingrese su numero de identificación"));
+
+console.log("hola,mi nombre es " + name);
+console.log("documento: " + type + " numero de documento " + number);
+
+
+
