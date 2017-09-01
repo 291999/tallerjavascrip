6608cacae925d0059ac48ab5d129f0185e092c55
EQUIPO <EQUIPO@ASUS>
2017-09-01 00:22:44 -0500

    terminado

--- public_html/P5/P5.js
+++
@@ -1,9 +0,0 @@
-var name=(prompt("ingrese su nombre: "));
-var type=(prompt("ingrese su tipo de documento: "));
-var number=(prompt("ingrese su numero de identificación"));
-
-console.log("hola,mi nombre es " + name);
-console.log("documento: " + type + " numero de documento " + number);
-
-
-
---
+++ public_html/P5/js/P5.js
@@ -0,0 +1,9 @@
+var name = (prompt("Enter your name: "));
+var type = (prompt("Enter your document type: "));
+var number = (prompt("Enter your identification number"));
+
+console.log("Hello my Name Is " + name);
+console.log("document: " + type + " Document number " + number);
+
+
+
