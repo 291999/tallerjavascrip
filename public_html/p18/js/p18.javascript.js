4b97f68c8ab6e4363247f5712808c306553e6ca0
EQUIPO <EQUIPO@ASUS>
2017-09-01 00:25:27 -0500

    terminado

---
+++ public_html/p18/js/p18.javascript.js
@@ -0,0 +1,9 @@
+var Salary = Number(prompt("Enter the worker's salary"));
+var porcentaje = Number(prompt("Enter retention percentage"));
+
+var retencion = (Salary - 19 / 100);
+console.log("Will be held" + retencion);
+var salary = (porcentaje - 19 / 100);
+console.log("You will be paid" + salary);
+
+
