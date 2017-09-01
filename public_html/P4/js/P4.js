89e41e2857f3ebda4f5d95f709ed8ab0a810bfaa
EQUIPO <EQUIPO@ASUS>
2017-09-01 00:22:36 -0500

    terminado

---
+++ public_html/P4/js/P4.js
@@ -0,0 +1,14 @@
+var name = (prompt("Enter your name: "));
+var type = (prompt("Enter the document type: "));
+var number = Number(prompt("Enter your identification number: "));
+var gender = (prompt("Enter your gender: "));
+var height = Number(prompt("Enter your height: "));
+var live = (prompt("It is true that he lives in Carthage? "));
+
+console.log("Hello my Name Is " + name);
+console.log("document " + type + " Document number " + number);
+console.log("My gender is " + gender);
+console.log("My height is " + height + " Mts");
+console.log("It is true " + live);
+
+
