a6bd0e0af69d1316141bbb3dd5194514c0c4f769
EQUIPO <EQUIPO@ASUS>
2017-09-01 00:25:18 -0500

    terminado

---
+++ public_html/p17/js/p17.javascript.js
@@ -0,0 +1,8 @@
+var a = Number(prompt("Enter number a"));
+var b = Number(prompt("Enter number b"));
+var c = Number(prompt("Enter number c"));
+var x = Number(prompt("Enter number x"));
+var a = (b);
+var c = (x);
+var resul = (x = c * b / a);
+console.log("The rule of three is" + resul);
