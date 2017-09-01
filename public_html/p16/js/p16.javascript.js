1f53692e5415df5400a600dfcccdad8c0e8c5536
EQUIPO <EQUIPO@ASUS>
2017-09-01 00:25:09 -0500

    terminado

---
+++ public_html/p16/js/p16.javascript.js
@@ -0,0 +1,8 @@
+var base = Number(prompt("Enter the rectangle base"));
+var altura = Number(prompt("Enter the rectangle base"));
+
+var perimetro = base + base + altura + altura;
+console.log("The perimeter of the rectangle is " + perimetro);
+
+var area = base * altura;
+console.log("The area of the rectangle is " + area);
