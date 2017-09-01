46845850fa4af6aacc4c2554f1ea822094163212
EQUIPO <EQUIPO@ASUS>
2017-09-01 00:24:39 -0500

    terminado

---
+++ public_html/p14/js/p14.javascript.js
@@ -0,0 +1,25 @@
+//punto 1
+
+var a = Number(prompt("Enter number of employees"));
+var resultado = 25832000 / a;
+console.log("Money Employees Will Receive " + resultado);
+
+//punto 2
+
+var n = Number(prompt("Enter the number of hours worked"));
+var cobra = Number = 45000 * n;
+var gasolina = Number = 20000;
+var resultado = (cobra - gasolina);
+console.log("The money he has left is" + resultado);
+
+//punto 3
+
+var centimetros = Number = 254;
+var pulgadas = Number = 12;
+var millas = Number = 5280;
+var resulto = (centimetros * pulgadas * millas);
+
+console.log("Centimeters in a mile is" + resultado);
+
+
+
