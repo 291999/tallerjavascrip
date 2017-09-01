36ba7283d12aee0e1d623e6a1f6cbc7e51d17322
EQUIPO <EQUIPO@ASUS>
2017-09-01 00:25:38 -0500

    terminado

---
+++ public_html/p20/js/p20.javascript.js
@@ -0,0 +1,6 @@
+var saliriominimo = Number(promtp("ingrese salario"));
+var aucilidetranporte = Number(promtp("ingrese aucilio de trasporte"));
+var retencios = Number(promtp("ingrese retencion"));
+var sueldodiario = Number(promtp("ingrese el suledo diario"));
+var porcentajedepago = Number(promtp("porcentaje de pago"));
+console.log
