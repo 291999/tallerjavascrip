64c33cfef1853347079fddcc4a8e669080b9b9a9
EQUIPO <EQUIPO@ASUS>
2017-09-01 00:25:59 -0500

    terminado

---
+++ public_html/p21/js/p21.javascript.js
@@ -0,0 +1,7 @@
+var hypo = Number(prompt("ingrese la ipotenusa"));
+var cateto1 = Number(prompt("ingrese el cateto"));
+var cateto2 = Number(prompt("ingres el segundo cateto"));
+var resul = hypo * cateto1 * cateto2;
+
+console.log("la hipotenusa del triangulo es " + resul);
+
