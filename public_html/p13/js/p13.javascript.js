cc4684ff77ae0d9635231b4198304ca883153a97
EQUIPO <EQUIPO@ASUS>
2017-09-01 00:24:29 -0500

    terminado

---
+++ public_html/p13/js/p13.javascript.js
@@ -0,0 +1,31 @@
+var espacio = "     ";
+var aste = "*";
+var z = "";
+var x = "";
+var c = " ";
+for (i = 1; i <= 13; i++) {
+  if (i === 1) {
+    console.log(z = espacio + aste);
+  }
+  if (i >= 2 && i <= 4) {
+    if (i === 3) {
+      x = z;
+    }
+    console.log(z = z.slice(1) + aste + aste);
+  }
+  if (i === 5) {
+    console.log(x);
+  }
+  if (i === 6) {
+    console.log(x);
+  }
+  if (i >= 7 && i <= 9) {
+    console.log(x = x.slice(1) + aste + aste);
+  }
+  if (i > 9) {
+    console.log(x = c + x.slice(0, -2));
+  }
+}
+
+
+
